655cba3513e1b180db8e37d48f1db271826978a4
Lucas Ault <lucas@lucasault.com>
2023-12-13 12:41:08 -0700

    Update for picamera2

--- wigglecam.py
+++ wigglecam.py
@@ -1,11 +1,19 @@
-from picamera2 import PiCamera2
-from time import sleep
+from picamera2 import Picamera2, Preview
+import time
+from datetime import datetime
 from gpiozero import Button
-
+from signal import pause
+picam2 = Picamera2()
 shutter_button = Button(20)
-camera = PiCamera2()
-
-camera.start_preview()
-shutter_button.wait_for_press()
-camera.capture('/home/pi/image.jpg')
-camera.stop_preview()
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+picam2.configure(camera_config)
+def capture():
+    picam2.start_preview(Preview.QTGL)
+    timestamp = datetime.now().isoformat()
+    picam2.start()
+    time.sleep(2)
+    picam2.capture_file('/home/pi/%s.jpg' % timestamp)
+    picam2.stop_preview()
+    picam2.stop()
+shutter_button.when_pressed = capture
+pause()
