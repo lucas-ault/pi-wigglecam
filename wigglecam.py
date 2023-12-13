78287d27c1fa5204483b555faae963f8690ab2af
Lucas Ault <lucas@lucasault.com>
2023-12-13 12:37:46 -0700

    Updating to picamera2

--- wigglecam.py
+++ wigglecam.py
@@ -1,9 +1,9 @@
-from picamera2 import PiCamera
+from picamera2 import PiCamera2
 from time import sleep
 from gpiozero import Button
 
 shutter_button = Button(20)
-camera = PiCamera()
+camera = PiCamera2()
 
 camera.start_preview()
 shutter_button.wait_for_press()
