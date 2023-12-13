3373f078bb6d12eebaa4c102791beea01a0f7db0
Lucas Ault <lucas@lucasault.com>
2023-12-13 12:33:47 -0700

    Adding python script

---
+++ wigglecam.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+from gpiozero import Button
+
+shutter_button = Button(20)
+camera = PiCamera()
+
+camera.start_preview()
+shutter_button.wait_for_press()
+camera.capture('/home/pi/image.jpg')
+camera.stop_preview()
